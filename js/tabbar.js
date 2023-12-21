5b02836fd640bfcd934b3ed0526de58a46859d32
Bogart <101217656+0xBogart@users.noreply.github.com>
2023-12-21 03:59:53 +0100

    Update tabbar.js

--- js/tabbar.js
+++ js/tabbar.js
@@ -1,16 +1,32 @@
 /* Tabla de Contenidos */
-  function toggleDropdown() {
+/*  function toggleDropdown() {
     var dropdown = document.getElementById("dropdownContent");
     var contenido = document.getElementById("contenido");
     /*var cerrarBtn = document.querySelector('.cerrar-btn');*/
 
-    if (dropdown.style.display === "none") {
+/*    if (dropdown.style.display === "none") {
       dropdown.style.display = "block";
       contenido.classList.add('show'); // Muestra el contenido cuando se despliega
       /*cerrarBtn.classList.add('show');*/ // Muestra el botón de cerrar
-    } else {
+/*    } else {
       dropdown.style.display = "none";
       contenido.classList.remove('show'); // Oculta el contenido cuando se cierra
-      /*cerrarBtn.classList.remove('show');*/ // Oculta el botón de cerrar
-    }
+      cerrarBtn.classList.remove('show');*/ // Oculta el botón de cerrar
+   /* }
+  } */
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("dropdownContent");
+  var contenido = document.getElementById("contenido");
+  /* var cerrarBtn = document.querySelector('.cerrar-btn'); */
+
+  if (dropdown.style.display === "" || dropdown.style.display === "none") {
+    dropdown.style.display = "block";
+    contenido.classList.add('show'); // Muestra el contenido cuando se despliega
+    /* cerrarBtn.classList.add('show'); */ // Muestra el botón de cerrar
+  } else {
+    dropdown.style.display = "none";
+    contenido.classList.remove('show'); // Oculta el contenido cuando se cierra
+    /* cerrarBtn.classList.remove('show'); */ // Oculta el botón de cerrar
   }
+}

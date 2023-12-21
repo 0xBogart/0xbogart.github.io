80f0ee60cd9a2f34b604c2c61bb2883f876a81dd
Bogart <101217656+0xBogart@users.noreply.github.com>
2023-12-21 03:20:51 +0100

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "200px";
-  document.getElementById("mySidebar").style.height = "200px";
+  document.getElementById("mySidebar").style.height = "550px";
   document.getElementById("main").style.marginLeft = "10px";
 }
 

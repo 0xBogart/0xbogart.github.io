9b641ddc2067ae825cb367aca2cda5c3fc243c61
Bogart <101217656+0xBogart@users.noreply.github.com>
2023-12-21 03:49:23 +0100

    Update tabbar.js

--- js/tabbar.js
+++ js/tabbar.js
@@ -2,15 +2,15 @@
   function toggleDropdown() {
     var dropdown = document.getElementById("dropdownContent");
     var contenido = document.getElementById("contenido");
-    var cerrarBtn = document.querySelector('.cerrar-btn');
+    /*var cerrarBtn = document.querySelector('.cerrar-btn');*/
 
     if (dropdown.style.display === "none") {
       dropdown.style.display = "block";
       contenido.classList.add('show'); // Muestra el contenido cuando se despliega
-      cerrarBtn.classList.add('show'); // Muestra el botón de cerrar
+      /*cerrarBtn.classList.add('show');*/ // Muestra el botón de cerrar
     } else {
       dropdown.style.display = "none";
       contenido.classList.remove('show'); // Oculta el contenido cuando se cierra
-      cerrarBtn.classList.remove('show'); // Oculta el botón de cerrar
+      /*cerrarBtn.classList.remove('show');*/ // Oculta el botón de cerrar
     }
   }

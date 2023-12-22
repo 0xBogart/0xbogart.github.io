db826d1fdca67a266ee27fdbb8c75c25616d9ec3
Bogart <101217656+0xBogart@users.noreply.github.com>
2023-12-22 03:58:50 +0100

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -10,3 +10,21 @@ function closeNav() {
   document.getElementById("mySidebar").style.border = "none";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+/*function toggleNav() {
+    var sidebar = document.getElementById("mySidebar");
+    var sidebarToggle = document.getElementById("sidebarToggle");
+
+    if (sidebar.style.width === "200px") {
+        sidebar.style.width = "0";
+        sidebar.style.border = "none"; // Eliminar el borde al cerrar
+        document.getElementById("main").style.marginLeft= "0";
+        sidebarToggle.innerHTML = "☠"; // Cambiar el ícono del botón al cerrar
+    } else {
+        sidebar.style.width = "200px";
+        sidebar.style.height = "550px";
+        sidebar.style.border = "3px solid red"; // Mantener el borde al abrir
+        document.getElementById("main").style.marginLeft = "10px";
+        sidebarToggle.innerHTML = "×"; // Cambiar el ícono del botón al abrir
+    }
+}*/

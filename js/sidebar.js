7f5f1fe82c81e068d8f2a321af3c5eee567985d9
Bogart <101217656+0xBogart@users.noreply.github.com>
2023-12-22 01:33:09 +0100

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,10 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "200px";
-  /*document.getElementById("mySidebar").style.height = "550px";*/
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidebar").style.height = "550px";
+  document.getElementById("main").style.marginLeft = "10px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("mySidebar").style.height = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
